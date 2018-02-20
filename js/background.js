554d41449f3d5703aaae5caa294c48bbc9b6e828
ajay2507 <pajay2507@gmail.com>
2018-02-19 20:28:43 -0800

    page action show icon  and add event listener

--- js/background.js
+++ js/background.js
@@ -1,5 +1,18 @@
-console.log(window.$_mod);
+console.log(window.$markoWidgetsConfig);
 
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request.marko) {
+      chrome.pageAction.show(sender.tab.id);
+      console.log("Built with marko"+sender.tab.url);
+    }
+  });
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+	console.log("onclick listener");
+  chrome.tabs.sendMessage(tab.id, {toggleOutlines: true});
+  chrome.tabs.executeScript(tab.id, {file:"js/highlight.js"});
+});
 
 function registerDeclaration(){
 	console.log("declaration");
--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 var selector = '[data-marko-key], [data-marko]';
 
-var elements = document.querySelector(selector);
-console.log(elements);
+var element = document.querySelector(selector);
+
+if(element || (markoWidgets != undefined && markoWidgets != null)){
+   console.log("Marko detected");
+    chrome.runtime.sendMessage({marko: true});
+}
+//console.log(elements);
