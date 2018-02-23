ad690127bd21569393fca607529d58595677fb46
ajay2507 <pajay2507@gmail.com>
2018-02-22 22:44:44 -0800

    added selectors to highlight components

--- js/highlightcomponents.js
+++ js/highlightcomponents.js
@@ -1,13 +1,14 @@
 if(selector.indexOf('[id]') == -1){
     selector = selector + ', [id]';  
    	componentNode = document.createElement('style');
-	componentNode.textContent = selector + ' {outline: solid 1px rgb(186, 78, 114);}';
+	componentNode.textContent = selector +","+"section"+ ' {outline: solid 1px rgb(186, 78, 114);}';
 	document.head.appendChild(componentNode);
 }else{
 	componentNode.remove();
 	componentNode = null;
 	let selector_array = selector.split(',');
-	selector = selector_array.splice(2).join('');
+	selector_array.splice(2);
+	selector = selector_array.join(',');
 	console.log("removed "+selector);
 }
 
